8c0beedc2ba4c3f68a851fec92a8781e40e4ec2f
John Costanza <jcostanza8@yahoo.com>
2015-06-08 21:46:15 -0400

    signup form

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -60,6 +60,13 @@ class UserTest < ActiveSupport::TestCase
     assert_not duplicate_user.valid?
   end
 
+  test "email addresses should be saved as lower-case" do
+    mixed_case_email = "Foo@ExAMPle.CoM"
+    @user.email = mixed_case_email
+    @user.save
+    assert_equal mixed_case_email.downcase, @user.reload.email
+  end
+
 
 # for passwords
   test "password should be present (nonblank)" do
